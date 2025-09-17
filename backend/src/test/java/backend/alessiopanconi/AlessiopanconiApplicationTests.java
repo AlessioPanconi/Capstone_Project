edf9f727e10b3f308af345e088c08ac7718c3f69
Alessio Panconi <apanconi94@gmail.com>
2025-09-17 11:06:36 +0200

    Aggiunta nuova cartella

---
+++ backend/src/test/java/backend/alessiopanconi/AlessiopanconiApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.alessiopanconi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlessiopanconiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
