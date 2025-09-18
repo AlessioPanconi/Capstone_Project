823be508ff1577d4a9ed298b3815b873663a6320
Alessio Panconi <apanconi94@gmail.com>
2025-09-18 13:14:00 +0200

    Avanzamento con controller di prenotazioni

--- backend/src/main/java/capstone/backend/backend/controllers/PrenotazioneController.java
+++ backend/src/main/java/capstone/backend/backend/controllers/PrenotazioneController.java
@@ -1,4 +1,40 @@
 package capstone.backend.backend.controllers;
 
+import capstone.backend.backend.entities.Prenotazione;
+import capstone.backend.backend.repositories.PrenotazioneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/prenotazioni")
 public class PrenotazioneController {
+
+    @Autowired
+    private PrenotazioneRepository prenotazioneRepository;
+
+    @PostMapping
+    public ResponseEntity<?> createPrenotazione(@RequestBody Prenotazione prenotazione) {
+        boolean esiste = prenotazioneRepository.existsByDataAndOra(prenotazione.getData(), prenotazione.getOra());
+        if (esiste) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body("L'orario selezionato non è disponibile. Scegli un altro slot.");
+        }
+
+        Prenotazione saved = prenotazioneRepository.save(prenotazione);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+    @GetMapping
+    public List<Prenotazione> getAllPrenotazioni() {
+        return prenotazioneRepository.findAll();
+    }
+    @GetMapping("/giorno/{data}")
+    public List<Prenotazione> getPrenotazioniByData(@PathVariable LocalDate data) {
+        return prenotazioneRepository.findByData(data);
+    }
 }
